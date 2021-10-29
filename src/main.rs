b2e5a0e47115581206ca4f0055440225317247a4
KokubunKazuya <kazup1125@me.com>
2021-10-29 21:12:08 +0900

    Fix: 直線検出＆描画処理

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() {
     };
 
     // 出力先の画像を定義
-    let mut output_img;
+    let output_img;
     let result_read_img = imread(SOURCE_IMAGE_PATH, IMREAD_COLOR);
     match result_read_img {
         Ok(img) => output_img = img,
@@ -40,23 +40,27 @@ fn main() {
     };
 
     // ハフ変換による直線検出
+    let mut line_img = output_img.clone();
     let mut lines= VectorOfVec4i::default();
-    let result_hough_lines = hough_lines_p(&edge_img, &mut lines, 1.0, CV_PI / 180.0, 50, 0.0, 1000.0);
+    let result_hough_lines= hough_lines_p(&edge_img, &mut lines, 1.0, CV_PI / 180.0, 250, 0.0, 1000.0);
     match result_hough_lines {
         Ok(_) => {
             // 線分を描画する
             for line_vec in lines.to_vec() {
-                line(&mut output_img,
+                line(&mut line_img,
                      Point::new(line_vec[0], line_vec[1]),
                      Point::new(line_vec[2], line_vec[3]),
-                     Scalar::new(0.0, 0.0, 255.0, 1.0), 5, 0, 0).ok();
+                     Scalar::new(0.0, 0.0, 255.0, 1.0),
+                     1,
+                     0,
+                     0
+                ).ok();
             }
+            imwrite("line.png", &line_img, &Vector::new()).ok();
         },
         Err(code) => {
             print!("code: {:?}", code);
             panic!();
         }
     }
-
-    imwrite("line.png", &output_img, &Vector::new());
 }
